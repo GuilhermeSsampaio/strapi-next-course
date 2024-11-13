8d85488523b1cfe93ee3cd7b29efda781cfa4d06
GuilhermeSsampaio <62760470+GuilhermeSsampaio@users.noreply.github.com>
2024-11-13 10:15:32 -0400

    subir

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,49 @@
+import { HeroSection } from "@/components/ui/custom/HeroSection";
+import { flattenAttributes } from "@/lib/utils";
+import qs from "qs";
+
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      on: {
+        "layout.hero-section": {
+          populate: {
+            image: {
+              fields: ["url", "alternativeText"],
+            },
+            link: {
+              populate: true,
+            },
+          },
+        },
+      },
+    },
+  },
+});
+
+async function getStrapiData(path: string) {
+  const baseUrl = "http://localhost:1337";
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
+  try {
+    const response = await fetch(url.href, { cache: "no-store" });
+    const data = await response.json();
+    const flattenedData = flattenAttributes(data);
+    console.dir(flattenedData, { depth: null });
+    return flattenedData;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default async function Home() {
+  const strapiData = await getStrapiData("/api/home-page");
+
+  const { title, description, blocks } = strapiData;
+  return (
+    <main className="container mx-auto py-6">
+      <HeroSection data={blocks[0]} />
+    </main>
+  );
+}
